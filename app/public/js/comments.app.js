872b34a18e4d72128857c77095ac700f19491088
malucia1 <70524347+malucia1@users.noreply.github.com>
2020-10-18 23:59:28 -0400

    UPDATED

--- app/public/js/comments.app.js
+++ app/public/js/comments.app.js
@@ -10,39 +10,45 @@ commentsApp = new Vue ({
       commentText: '',
     }
   },
-  methods: {
-    fetchUser(){
-      fetch('api/comments/')
-      .then(response => response.json())
-      .then(json => {
-        this.comments=json;
-        console.log(this.comments);
-      });
-    },
-    createComment(){
-      fetch('api/comments/create.php', {
-        method: 'POST',
-        body: JSON.stringify(this.commentNew),
-        headers: {
-          "Content-Type": "application/json; charset=utf-8"
+
+
+  methods:{
+      fetchUser(){
+        fetch('api/comments/')
+        .then(response => response.json())
+        .then(json => {
+          this.comments=json;
+          console.log(this.comments);
+        });
+      },
+
+      createComment() {
+        fetch('api/comments/create.php', {
+          method:'POST',
+          body: JSON.stringify(this.commentNew),
+          headers: {
+            "Content-Type": "application/json; charset=utf-8"
+          }
+        })
+        .then( response => response.json() )
+        .then( json => {
+          console.log("Returned from post:", json);
+          this.comments = json;
+          this.commentNew = this.newCommentData();
+        });
+
+        console.log("Creating (POSTing)...!");
+        console.log(this.commentNew);
+      },
+      newCommentData() {
+        return {
+          id: "",
+          commentText: ""
         }
-      })
-      .then(response => response.json() )
-      .then( json => {
-        console.log("Returned from post: ", json);
-        this.comments.push(json[0]);
-        this.commentNew = this.commentNewData();
-      });
-      console.log("Creating (POSTing)...!");
-      console.log(this.commentNew);
-    },
-    commentNewData(){
-      return{
-        id: '',
-        commentText: ''
       }
+
     },
-    created(){
+    created() {
       this.fetchUser();
     }
-});
+  });
