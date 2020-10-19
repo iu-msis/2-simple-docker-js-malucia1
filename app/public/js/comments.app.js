27fabd75afc0dfed39e5294dacc5f09c155f83a8
malucia1 <70524347+malucia1@users.noreply.github.com>
2020-10-19 19:57:12 -0400

    HTML, JS
    HTML, JS

--- app/public/js/comments.app.js
+++ app/public/js/comments.app.js
@@ -1,54 +1,53 @@
-commentsApp = new Vue ({
+var app = new Vue({
   el: '#commentsTableID',
   data: {
-    comments: [{
+    commentList: [{
       id: '',
-      commentText: '',
+      commentText: ''
     }],
-    commentNew: {
+    newComment: {
       id: '',
-      commentText: '',
+      commentText: ''
     }
   },
 
-
   methods:{
-      fetchUser(){
-        fetch('api/comments/')
-        .then(response => response.json())
-        .then(json => {
-          this.comments=json;
-          console.log(this.comments);
-        });
-      },
-
-      createComment() {
-        fetch('api/comments/create.php', {
-          method:'POST',
-          body: JSON.stringify(this.commentNew),
-          headers: {
-            "Content-Type": "application/json; charset=utf-8"
-          }
-        })
-        .then( response => response.json() )
-        .then( json => {
-          console.log("Returned from post:", json);
-          this.comments = json;
-          this.commentNew = this.newCommentData();
-        });
-
-        console.log("Creating (POSTing)...!");
-        console.log(this.commentNew);
-      },
-      newCommentData() {
-        return {
-          id: "",
-          commentText: ""
-        }
-      }
-
+    fetchUser(){
+      fetch('api/comments/')
+      .then(response => response.json())
+      .then(json => {
+        this.commentList=json;
+        console.log(this.commentList);
+      });
     },
-    created() {
-      this.fetchUser();
+
+    createComment() {
+      fetch('api/comments/create.php', {
+        method:'POST',
+        body: JSON.stringify(this.newComment),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      })
+      .then( response => response.json() )
+      .then( json => {
+        console.log("Returned from post:", json);
+        this.commentList = json;
+        this.newComment = this.newCommentData();
+      });
+
+      console.log("Creating (POSTing)...!");
+      console.log(this.newComment);
+    },
+    newCommentData() {
+      return {
+        id: "",
+        commentText: ""
+      }
     }
-  });
+
+  },
+  created() {
+    this.fetchUser();
+  }
+});
